c7c4db093cec14f3e9fe44a760ca9d4541ca1b05
jopinta <tatinfa@yahoo.com>
2022-02-05 15:38:11 -0800

    app_views

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -16,4 +16,17 @@ from api.v1.views import app_views
 @app_views.route('/returnstuff')
 def returnstuff():
     '''return stuff'''
-    return jsonify({'output' : num_dict})
+    return jsonify(teturnstuff='200')
+
+
+@app_views.route('/stuff')
+def stuff():
+    '''JSON Responses'''
+    todos = {
+        'states': State, 'users': User
+        'amenities': Amenity, 'cities': City,
+        'places': Place, 'reviews': Review
+    }
+    for key in todos:
+        todos[key] = storage.count(todos[key])
+    return jsonfy(todos)
