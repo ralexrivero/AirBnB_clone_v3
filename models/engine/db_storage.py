8ed8d2e0b061ddd7a1631035ecafc03700cf5c04
jopinta <tatinfa@yahoo.com>
2022-02-05 18:15:13 -0800

    methos to db

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -77,4 +77,16 @@ class DBStorage:
 
     def get(self, cls, id):
         '''method to retrieve one object'''
+        if cls and id:
+            tempo = cls,__name__ + "." + id
+            count = self.all(cls)
+            for key in count:
+                if key == tempo:
+                    return count[key]
+        else:
+            return None
+
+    def count(self, cls=None):
+        '''class (optional)'''
+            return (len(self.all(cls)))
         
