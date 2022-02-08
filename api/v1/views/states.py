85c8d4e4ac40ed29dbe739e587657d160b9b4761
ralexrivero <ralexrivero@gmail.com>
2022-02-08 00:34:15 +0000

    fix style but not E722 bare except

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-''' new view for State objects''' 
+''' new view for State objects'''
 
 from flask import Flask
 
+
 @app_views.route('/status', methods=['GET'] strict_slashes=False)
-#GET /api/v1/states
+"""GET /api/v1/states"""
+
+
 def toGet():
     '''getting thing'''
     objects = storage.all('State')
     lista = []
     for state in objects.values():
-    lista.append(state.to_dict())
+        lista.append(state.to_dict())
     return jsonify(lista)
 
 @app_views.route('/states/<_id>', methods=['GET'],
                  strict_slashes=False)
-#GET /api/v1/states/<state_id>
+"""GET /api/v1/states/<state_id>"""
+
 
 def toGetid():
     '''Updates a State object id'''
@@ -23,6 +27,7 @@ def toGetid():
     if objects is None:
         abort(404)
     return jsonify(objects.to_dict()), 'OK'
+
 
 '''
 DELETE /api/v1/states/<state_id>
