a8ecbde797731fcf958f0bdbc25d0159f3844beb
jopinta <tatinfa@yahoo.com>
2022-02-08 13:06:51 -0800

    repeted library

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,7 +6,7 @@ from flask import Flask, jsonify
 from models import storage
 from os import getenv
 from api.v1.views import app_views
-from os import getenv
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
@@ -30,5 +30,5 @@ if __name__ == "__main__":
     if not host:
         host = '0.0.0.0'
     if not port:
-        port = 5000
+        port = '5000'
     app.run(host=host, port=port, threaded=True)
