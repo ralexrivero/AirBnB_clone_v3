c1105adb9a8fb178733594e50593cde7b9417c66
jopinta <tatinfa@yahoo.com>
2022-02-08 13:22:35 -0800

    test with no lib

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/python3xx
 '''api status'''
 import models
 from models import storage
-from models.amenity import Amenity
 from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 from flask import jsonify
 from api.v1.views import app_views
 
@@ -22,11 +16,9 @@ def returnstuff():
 @app_views.route('/stats', strict_slashes=False)
 def stuff():
     '''JSON Responses'''
-    todos = {
-        'states': State, 'users': User,
-        'amenities': Amenity, 'cities': City,
-        'places': Place, 'reviews': Review
-    }
+    todos = {'states': State, 'users': User,
+            'amenities': Amenity, 'cities': City,
+            'places': Place, 'reviews': Review}
     for key in todos:
         todos[key] = storage.count(todos[key])
     return jsonify(todos)
