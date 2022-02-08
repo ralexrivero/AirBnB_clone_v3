7463de375d2e9c1a6bfe3c3df5df335a73fdc457
jopinta <tatinfa@yahoo.com>
2022-02-08 13:04:24 -0800

    port and host

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 '''Flask server (variable app)'''
 
+
+from flask import Flask, jsonify
 from models import storage
 from os import getenv
 from api.v1.views import app_views
-from flask import Flask, jsonify
-
+from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
@@ -20,16 +21,14 @@ def downtear(exception):
 @app.errorhandler(404)
 def page_not_found(error):
     '''return render_template'''
-    return jsonify({'error': 'Not found'}), 404
+    return jsonify('error': 'Not found'), 404
 
 
 if __name__ == "__main__":
-    if getenv('HBNB_API_HOST'):
-        host = getenv('HBNB_API_HOST')
-    else:
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
         host = '0.0.0.0'
-    if getenv('HBNB_API_PORT'):
-        port = getenv('HBNB_API_PORT')
-    else:
+    if not port:
         port = 5000
     app.run(host=host, port=port, threaded=True)
