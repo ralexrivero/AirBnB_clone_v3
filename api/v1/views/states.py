dc87ec1e956cbbe36bb7fac37e18e2fec23faf47
jopinta <tatinfa@yahoo.com>
2022-02-08 08:54:50 -0800

    put post get

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,6 +2,12 @@
 ''' new view for State objects'''
 
 from flask import Flask
+from flask import Flask, abort
+from api.v1.views import app_views
+from os import name
+from models.state import State
+from flask import request
+
 
 
 @app_views.route('/status', methods=['GET'] strict_slashes=False)
@@ -29,12 +35,41 @@ def toGetid():
     return jsonify(objects.to_dict()), 'OK'
 
 
-'''
-DELETE /api/v1/states/<state_id>
+#DELETE /api/v1/states/<state_id>
 
-POST /api/v1/states
+#POST /api/v1/states
+@app_views.route('/states/', methods=['POST'],
+                 strict_slashes=False)
+def posting():
+    '''Creates a State'''
+    response = request.get_json()
+    if response id None:
+        abort(400, {'Not a JSON'})
+    if "name" not in response:
+        abort(400, {'Missing name'})
+    stateObject = State(name=response['name'])
+    storage.new(stateObject)
+    storage.save()
+    return jsonify(stateObject.to_dict()), '201'
 
+
+@app_views.route('/states/<state_id>', methods=['PUT'],
+                 strict_slashes=False)
+def putinV():
+    '''vladimir'''
+    response = request.get_json()
+    if response id None:
+        abort(400, {'Not a JSON'})
+    stateObject = storage.get(State, state_id)
+    if stateObject is None:
+        abort(404)
+    ignoreKeys = ['id', 'created_at', 'updated_at']
+    for key in response.items():
+        if key not in ignoreKeys:
+            setattr(stateObject, key)
+    storage.save()
+    return jsonify(stateObject.to_dict()), '200'
 request.get_json
 
-PUT /api/v1/states/<state_id>
-'''
+#PUT /api/v1/states/<state_id>
+
